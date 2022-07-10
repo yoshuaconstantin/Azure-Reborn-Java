24d7953c72e77383bce8ae0526bc85168d4f0770
r0th Baby <joshuaconstantine.k@gmail.com>
2022-07-10 23:10:09 +0700

    0.004
    Create buggy widget. must fix

--- app/src/main/java/com/joshua/r0th/azuretw/MainActivity.java
+++ app/src/main/java/com/joshua/r0th/azuretw/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 
 import android.animation.ObjectAnimator;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
@@ -20,6 +23,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
+import android.widget.RemoteViews;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
@@ -35,6 +39,7 @@ import com.joshua.r0th.azuretw.root_utils.Checking;
 import com.joshua.r0th.azuretw.sd888_param.SDCPU_TWEAK;
 import com.joshua.r0th.azuretw.tweak.advanched_tweak;
 import com.joshua.r0th.azuretw.tweak.memory_tweak;
+import com.joshua.r0th.azuretw.widget.Azure_widget_provider;
 
 import org.w3c.dom.Text;
 
@@ -69,20 +74,21 @@ public class MainActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+
         sheetBehave();
         final RootUtils.SU su = new RootUtils.SU(false, null);
-        sharedpreferences = getSharedPreferences("myref",Context.MODE_PRIVATE);
+        sharedpreferences = getSharedPreferences("myref", Context.MODE_PRIVATE);
         proctype = sharedpreferences.getString("ProcType", null);
-        modeClick(binding.carview1,proctype,"Chill",R.drawable.defaulton,R.drawable.defaultoff,R.drawable.defaultoff,1);
-        modeClick(binding.carview2,proctype,"Smart",R.drawable.defaultoff,R.drawable.defaulton,R.drawable.defaultoff,2);
-        modeClick(binding.carview4,proctype,"Fast AF",R.drawable.defaultoff,R.drawable.defaultoff,R.drawable.defaulton,4);
+        modeClick(binding.carview1, proctype, "CHILL", R.drawable.defaulton, R.drawable.defaultoff, R.drawable.defaultoff, 1);
+        modeClick(binding.carview2, proctype, "Smart Casual", R.drawable.defaultoff, R.drawable.defaulton, R.drawable.defaultoff, 2);
+        modeClick(binding.carview4, proctype, "Fast AF", R.drawable.defaultoff, R.drawable.defaultoff, R.drawable.defaulton, 4);
         binding.textmode.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                switch (proctype){
+                switch (proctype) {
                     case "SM350":
                         sdCPU_TWEAK.defaultSD();
-                        statusEnabled("Tap to enable","Tap to enable","Tap to enable");
+                        statusEnabled("Tap to enable", "Tap to enable", "Tap to enable");
                         break;
                     case "MTK1200":
                         //mtkcpu_tweak.defaultMTK();
@@ -91,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
                         break;
                 }
                 binding.textmode.setText("DEFAULT");
-                statusMode(R.drawable.defaultoff,R.drawable.defaultoff,R.drawable.defaultoff);
+                statusMode(R.drawable.defaultoff, R.drawable.defaultoff, R.drawable.defaultoff);
 
             }
         });
@@ -106,51 +112,52 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        sharedpreferences = getSharedPreferences("myref",Context.MODE_PRIVATE);
+        updateWidget(MainActivity.this);
+        sharedpreferences = getSharedPreferences("myref", Context.MODE_PRIVATE);
         String tutorialDialog = RootUtils.getProp("AzureWelcome");
         String RootChecking = sharedpreferences.getString("passed", null);
-        if (RootChecking.isEmpty() || RootChecking == null){
+        if (RootChecking.isEmpty() || RootChecking == null) {
             checkingMod.dialogAlert(MainActivity.this,
                     "No Root Detected, Please Grant SU / Root Your phone");
             binding.textmode.setText("ERROR NO ROOT");
             binding.textmode.setTextSize(15);
             binding.relnew1.setVisibility(View.GONE);
             binding.relnew2.setVisibility(View.GONE);
-        }else {
+        } else {
             binding.relnew1.setVisibility(View.VISIBLE);
             binding.relnew2.setVisibility(View.VISIBLE);
             final String checking = RootUtils.getProp("azureSDmode");
-            switch (checking){
+            switch (checking) {
                 case "1":
                     binding.textmode.setText("CHILL");
-                    statusMode(R.drawable.defaulton,R.drawable.defaultoff,R.drawable.defaultoff);
-                    statusEnabled("Enabled","Tap to Enable","Tap to enable");
+                    statusMode(R.drawable.defaulton, R.drawable.defaultoff, R.drawable.defaultoff);
+                    statusEnabled("Enabled", "Tap to Enable", "Tap to enable");
                     break;
                 case "2":
                     binding.textmode.setText("Smart Casual");
-                    statusMode(R.drawable.defaultoff,R.drawable.defaulton,R.drawable.defaultoff);
-                    statusEnabled("Tap to enable","Enabled","Tap to enable");
+                    statusMode(R.drawable.defaultoff, R.drawable.defaulton, R.drawable.defaultoff);
+                    statusEnabled("Tap to enable", "Enabled", "Tap to enable");
                     break;
                 case "4":
                     binding.textmode.setText("Fast AF");
-                    statusMode(R.drawable.defaultoff,R.drawable.defaultoff,R.drawable.defaulton);
-                    statusEnabled("Tap to enable","Tap to enable","Enabled");
+                    statusMode(R.drawable.defaultoff, R.drawable.defaultoff, R.drawable.defaulton);
+                    statusEnabled("Tap to enable", "Tap to enable", "Enabled");
                     break;
                 case "5":
                     binding.textmode.setText("DEFAULT");
-                    statusMode(R.drawable.defaultoff,R.drawable.defaultoff,R.drawable.defaultoff);
-                    statusEnabled("Tap to enable","Tap to enable","Tap to enable");
+                    statusMode(R.drawable.defaultoff, R.drawable.defaultoff, R.drawable.defaultoff);
+                    statusEnabled("Tap to enable", "Tap to enable", "Tap to enable");
                     break;
             }
         }
-        if (tutorialDialog.isEmpty()){
+        if (tutorialDialog.isEmpty()) {
             flipperDialog();
             RootUtils.setProp("AzureWelcome 1");
         }
 
     }
 
-    private void statusMode(int chill, int smartcas, int fastaf){
+    private void statusMode(int chill, int smartcas, int fastaf) {
         binding.chillstatus.setImageResource(chill);
         binding.smartstatus.setImageResource(smartcas);
         binding.faststatus.setImageResource(fastaf);
@@ -158,28 +165,29 @@ public class MainActivity extends AppCompatActivity {
 
     private void modeClick(NeumorphCardView button, String check, String ModeText,
                            int chill, int smart, int fast,
-                           int number ){
+                           int number) {
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                switch (check){
+                switch (check) {
                     case "SM350":
-                        switch (number){
+                        switch (number) {
                             case 1:
                                 sdCPU_TWEAK.chill();
-                                statusEnabled("Enabled","Tap to Enable","Tap to enable");
+                                statusEnabled("Enabled", "Tap to Enable", "Tap to enable");
                                 break;
                             case 2:
                                 sdCPU_TWEAK.smarcasual();
-                                statusEnabled("Tap to enable","Enabled","Tap to enable");
+                                statusEnabled("Tap to enable", "Enabled", "Tap to enable");
                                 break;
                             case 4:
                                 sdCPU_TWEAK.fastaf();
-                                statusEnabled("Tap to enable","Tap to enable","Enabled");
+                                statusEnabled("Tap to enable", "Tap to enable", "Enabled");
                                 break;
-                            case 5 :
+                            case 5:
                                 sdCPU_TWEAK.defaultSD();
-                                statusEnabled("Tap to enable","Tap to enable","Tap to enable");
+                                statusEnabled("Tap to enable", "Tap to enable", "Tap to enable");
                                 break;
                             default:
                                 break;
@@ -191,14 +199,25 @@ public class MainActivity extends AppCompatActivity {
                     default:
                         break;
                 }
+                handler = new Handler();
+                r = new Runnable() {
 
+                    @Override
+                    public void run() {
+                        // TODO Auto-generated method stub
+                        updateWidget(MainActivity.this);
+                        handler.postDelayed(this, 1000);
+                    }
+                };
+                handler.postDelayed(r, 0);
                 binding.textmode.setText(ModeText);
-                statusMode(chill,smart,fast);
+                statusMode(chill, smart, fast);
 
 
             }
         });
     }
+
     @Override
     public void onBackPressed() {
         moveTaskToBack(true);
@@ -220,22 +239,23 @@ public class MainActivity extends AppCompatActivity {
         handler.postDelayed(r, 0);
     }
 
-    private void systeminfo(String proctype){
+    private void systeminfo(String proctype) {
 //        RandomAccessFile reader = new RandomAccessFile("/sys/devices/system/cpu/cpu0/cpufreq/cpu_temp", "r");
 //        String line = reader.readLine();
         proctype = sharedpreferences.getString("ProcType", null);
-                sys.CPU_info(proctype,binding.ProcType,binding.deviceName,binding.silvercoreFreq,binding.goldcoreFreq,
-                        binding.primecoreFreq,binding.gpumaxFreq,binding.memFree,binding.memoryUsed,
-                        binding.BatteryTemp,binding.CPUtemp, this);
+        sys.CPU_info(proctype, binding.ProcType, binding.deviceName, binding.silvercoreFreq, binding.goldcoreFreq,
+                binding.primecoreFreq, binding.gpumaxFreq, binding.memFree, binding.memoryUsed,
+                binding.BatteryTemp, binding.CPUtemp, this);
 
     }
 
-    private void statusEnabled(String text1, String text2, String text3){
+    private void statusEnabled(String text1, String text2, String text3) {
         binding.chillstatustextactived.setText(text1);
         binding.smartstatustextactived.setText(text2);
         binding.faststatustextactived.setText(text3);
     }
-    private void flipperDialog(){
+
+    private void flipperDialog() {
         dialogBuilder = new AlertDialog.Builder(MainActivity.this);
         final View tutorialDialog = getLayoutInflater().inflate(R.layout.tutorial_layout, null);
         ImageButton next = tutorialDialog.findViewById(R.id.buttonNext);
@@ -244,24 +264,25 @@ public class MainActivity extends AppCompatActivity {
         dialogBuilder.setView(tutorialDialog);
         dialog = dialogBuilder.create();
         dialog.show();
-        
+
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                flipper.setInAnimation(MainActivity.this,R.anim.slide_in_right);
-                flipper.setOutAnimation(MainActivity.this,R.anim.slide_out_left);
+                flipper.setInAnimation(MainActivity.this, R.anim.slide_in_right);
+                flipper.setOutAnimation(MainActivity.this, R.anim.slide_out_left);
                 flipper.showNext();
             }
         });
         prev.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                flipper.setInAnimation(MainActivity.this,android.R.anim.slide_in_left);
-                flipper.setOutAnimation(MainActivity.this,android.R.anim.slide_out_right);
+                flipper.setInAnimation(MainActivity.this, android.R.anim.slide_in_left);
+                flipper.setOutAnimation(MainActivity.this, android.R.anim.slide_out_right);
                 flipper.showPrevious();
             }
         });
     }
+
     private void sheetBehave() {
 
         this.linearLayoutBSheet = findViewById(R.id.bottomSheet);
@@ -269,20 +290,20 @@ public class MainActivity extends AppCompatActivity {
         bottomSheetBehavior.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
             public void onStateChanged(View view, int newState) {
-            view.findViewById(R.id.bottomSheet).setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (stateBehave == 1){
-                        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
-                        stateBehave++;
-                    }else if (stateBehave == 2){
-                        bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
-                        stateBehave--;
+                view.findViewById(R.id.bottomSheet).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        if (stateBehave == 1) {
+                            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                            stateBehave++;
+                        } else if (stateBehave == 2) {
+                            bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                            stateBehave--;
+                        }
                     }
-                }
-            });
-                if(newState == BottomSheetBehavior.STATE_EXPANDED){
-                    stateBehave=2;
+                });
+                if (newState == BottomSheetBehavior.STATE_EXPANDED) {
+                    stateBehave = 2;
                     view.findViewById(R.id.reldoze).setOnClickListener(new View.OnClickListener() {
                         @Override
                         public void onClick(View view) {
@@ -298,8 +319,8 @@ public class MainActivity extends AppCompatActivity {
 //                            startActivity(intent);
                         }
                     });
-                }else if(newState == BottomSheetBehavior.STATE_COLLAPSED){
-                stateBehave=1;
+                } else if (newState == BottomSheetBehavior.STATE_COLLAPSED) {
+                    stateBehave = 1;
                 }
             }
 
@@ -311,7 +332,37 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    private void updateWidget(Context context) {
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
 
 
-}
+        final String checking = RootUtils.getProp("azureSDmode");
+        switch (checking){
+            case "1":
+                views.setTextViewText(R.id.widget_profile_text, "CHILL");
+                views.setImageViewResource(R.id.widget_img, R.drawable.chillvector);
+                break;
+            case "2":
+                views.setTextViewText(R.id.widget_profile_text, "Smart");
+                views.setImageViewResource(R.id.widget_img, R.drawable.smartvector);
+                break;
+            case "4":
+                views.setTextViewText(R.id.widget_profile_text, "Fast AF");
+                views.setImageViewResource(R.id.widget_img, R.drawable.speed);
+                break;
+            case "5":
+                views.setTextViewText(R.id.widget_profile_text, "DEFAULT");
+                views.setImageViewResource(R.id.widget_img, R.drawable.azurenewlogo_round);
+                break;
+        }
+
+
+        // Tell the AppWidgetManager to perform an update on the current app widget
+        ComponentName theWidget = new ComponentName(this, Azure_widget_provider.class);
+        AppWidgetManager manager = AppWidgetManager.getInstance(this);
+        manager.updateAppWidget(theWidget, views);
+        }
+
+    }
+
 
---
+++ app/src/main/java/com/joshua/r0th/azuretw/widget/Azure_widget_provider.java
@@ -0,0 +1,37 @@
+package com.joshua.r0th.azuretw.widget;
+
+import android.annotation.SuppressLint;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.joshua.r0th.azuretw.MainActivity;
+import com.joshua.r0th.azuretw.R;
+import com.joshua.r0th.azuretw.root_utils.RootUtils;
+
+public class Azure_widget_provider extends AppWidgetProvider {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.widget);
+            views.setOnClickPendingIntent(R.id.relbutton, pendingIntent);
+
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+}
